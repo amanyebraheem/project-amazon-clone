f3821fb5ef98020c72e3a4a8f2f280a42ee7821e
amanyebraheem <amanyebraheem9900@gmail.com>
2025-06-13 08:10:18 +0300

    first project-all-clone

--- app/lib/stripe.ts
+++ app/lib/stripe.ts
@@ -1,10 +1,11 @@
-// import Stripe from "stripe"
+import Stripe from "stripe"
 
 // export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 
-import Stripe from "stripe";
+// import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-05-28.basil", 
-});
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+//   apiVersion: "2025-05-28.basil", 
+// });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
