2308300e0fb55dc0b392f11115f4d5dcc931c7da
amanyebraheem <amanyebraheem9900@gmail.com>
2025-06-13 08:26:30 +0300

    all-project-amazon-clone-one

--- app/lib/stripe.ts
+++ app/lib/stripe.ts
@@ -3,14 +3,10 @@
 // // export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 
-// // import Stripe from "stripe";
-
-// // const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-// //   apiVersion: "2025-05-28.basil", 
-// // });
-// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-import Stripe from 'stripe';
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: "2025-05-28.basil", 
 });
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
